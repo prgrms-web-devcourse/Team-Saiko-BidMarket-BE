b3256281b352516a5ceac9c18a99f2668a2acc65
waterfog9580 <waterfog9580@gmail.com>
2022-07-25 23:12:16 +0900

    [BM-52] :sparkles: User 관련 엔티티 정의

---
+++ src/main/java/com/saiko/bidmarket/user/entity/Group.java
@@ -0,0 +1,57 @@
+package com.saiko.bidmarket.user.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+@Entity
+public class Group {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Column(length = 20)
+  private String name;
+
+  @OneToMany(mappedBy = "group")
+  private List<GroupPermission> permissions = new ArrayList<>();
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<GrantedAuthority> getAuthorities() {
+    return permissions.stream()
+                      .map(gp -> new SimpleGrantedAuthority(gp.getPermission().getName()))
+                      .collect(Collectors.toList());
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+        .append("id", id)
+        .append("name", name)
+        .append("permissions", permissions)
+        .toString();
+  }
+
+}
---
+++ src/main/java/com/saiko/bidmarket/user/entity/User.java
@@ -0,0 +1,61 @@
+package com.saiko.bidmarket.user.entity;
+
+import static org.apache.commons.lang3.StringUtils.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.util.Assert;
+
+import com.saiko.bidmarket.common.entity.BaseTime;
+import com.sun.istack.NotNull;
+
+@Entity
+public class User extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank
+  @Column(length = 64, unique = true)
+  private String email;
+
+  @NotBlank
+  @Column(length = 20)
+  private String nickname;
+
+  @NotBlank
+  @Column(length = 512)
+  private String profileImage;
+
+  @NotBlank
+  @Column(length = 20)
+  private String provider;
+
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "group_id")
+  private Group group;
+
+  protected User() {/*no-op*/}
+
+  public User(String email, String nickname, String profileImage, String provider, Group group) {
+    Assert.isTrue(isNotBlank(email), "Email must be provided");
+    Assert.isTrue(isNotBlank(nickname), "Nickname must be provided");
+    Assert.isTrue(isNotBlank(profileImage), "ProfileImage must be provided");
+    Assert.isTrue(isNotBlank(provider), "ProfileImage must be provided");
+    Assert.notNull(group, "Group must be provided");
+
+    this.email = email;
+    this.nickname = nickname;
+    this.profileImage = profileImage;
+    this.provider = provider;
+    this.group = group;
+  }
+}
