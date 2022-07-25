b18aad229a956e14e801b4f49e799a8d67ec8125
gkdud583 <hyng0115@gmail.com>
2022-07-25 17:00:47 +0900

    [BM-64] :sparkles: 상품, 이미지 엔티티 생성

---
+++ src/main/java/com/saiko/bidmarket/product/entity/Image.java
@@ -0,0 +1,31 @@
+package com.saiko.bidmarket.product.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.saiko.bidmarket.common.entity.BaseTime;
+
+@Entity
+public class Image extends BaseTime {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  @Column(nullable = false)
+  private String url;
+
+  private boolean isThumbnail = false;
+
+  protected Image() {
+  }
+}
