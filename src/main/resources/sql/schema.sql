4c701813402885a828f030d078eb743f0bcda029
ecvheo1 <ecvheo@naver.com>
2022-07-26 17:00:30 +0900

    [BM-113] :memo: DB 스키마 세팅 스크립트 생성

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `product` (
+    id bigint not null auto_increment,
+    title varchar(16) not null,
+    description varchar(500) not null,
+    minimum_price int not null,
+    category varchar(100) not null,
+    location varchar(100),
+    expire_at datetime not null,
+    created_at datetime,
+    updated_at datetime,
+    primary key (id)
+);
+
+CREATE TABLE `image` (
+    id bigint not null auto_increment,
+    product_id bigint,
+    url varchar(512) not null,
+    `order` int not null,
+    created_at datetime,
+    updated_at datetime,
+    primary key (id)
+);
