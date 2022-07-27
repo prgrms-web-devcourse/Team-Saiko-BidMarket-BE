8c96dc251d2d34d8a6900c638dff57252af81656
waterfog9580 <waterfog9580@gmail.com>
2022-07-27 10:42:58 +0900

    [BM-52] :sparkles: NotfoundException 예외 추가

---
+++ src/main/java/com/saiko/bidmarket/common/exception/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.saiko.bidmarket.common.exception;
+
+public class NotFoundException extends RuntimeException {
+
+  public NotFoundException() {
+    super();
+  }
+
+  public NotFoundException(String message) {
+    super(message);
+  }
+
+}
