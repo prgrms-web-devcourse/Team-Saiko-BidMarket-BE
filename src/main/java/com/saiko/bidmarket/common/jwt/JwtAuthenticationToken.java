c8c15551657c58e15d5f0752906bceabdd7ccca4
UJ15 <dbwogml15@naver.com>
2022-07-29 19:06:56 +0900

    [BM-116] :adhesive_bandage: 접근지정자

--- src/main/java/com/saiko/bidmarket/common/jwt/JwtAuthenticationToken.java
+++ src/main/java/com/saiko/bidmarket/common/jwt/JwtAuthenticationToken.java
@@ -22,7 +22,7 @@ public class JwtAuthenticationToken extends AbstractAuthenticationToken {
     this.credentials = credentials;
   }
 
-  JwtAuthenticationToken(
+  public JwtAuthenticationToken(
       Object principal,
       String credentials,
       Collection<? extends GrantedAuthority> authorities) {
