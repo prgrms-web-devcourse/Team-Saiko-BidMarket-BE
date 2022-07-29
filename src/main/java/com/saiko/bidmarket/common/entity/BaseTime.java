3236afef074ec87b171a49d85e71a5ddcd9762f0
gkdud583 <hyng0115@gmail.com>
2022-07-29 18:38:26 +0900

    [BM-60] :adhesive_bandage: BaseTime entity 에 어노테이션 추가

--- src/main/java/com/saiko/bidmarket/common/entity/BaseTime.java
+++ src/main/java/com/saiko/bidmarket/common/entity/BaseTime.java
@@ -2,12 +2,15 @@ package com.saiko.bidmarket.common.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseTime {
 
   @CreatedDate
