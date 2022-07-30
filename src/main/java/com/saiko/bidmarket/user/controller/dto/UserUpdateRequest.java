c91f6226c1b24a334eb07096bea8127b7f82a68d
UJ15 <dbwogml15@naver.com>
2022-07-30 15:29:48 +0900

    [BM-142] :adhesive_bandage: NotBlank, Nullable 추가

--- src/main/java/com/saiko/bidmarket/user/controller/dto/UserUpdateRequest.java
+++ src/main/java/com/saiko/bidmarket/user/controller/dto/UserUpdateRequest.java
@@ -1,10 +1,16 @@
 package com.saiko.bidmarket.user.controller.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.Length;
+import org.springframework.lang.Nullable;
 
 public class UserUpdateRequest {
 
   @Length(max = 20)
+  @NotBlank
+  @Nullable
   private final String username;
 
   private final String image;
