25ef5cb908e3a918a1c783c81e47d70767bb469a
UJ15 <dbwogml15@naver.com>
2022-07-30 15:39:22 +0900

    [BM-142] :adhesive_bandage: Nullable 삭제 변수명 변경

--- src/main/java/com/saiko/bidmarket/user/controller/dto/UserUpdateRequest.java
+++ src/main/java/com/saiko/bidmarket/user/controller/dto/UserUpdateRequest.java
@@ -1,31 +1,28 @@
 package com.saiko.bidmarket.user.controller.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
-import org.springframework.lang.Nullable;
 
 public class UserUpdateRequest {
 
   @Length(max = 20)
   @NotBlank
-  @Nullable
   private final String username;
 
-  private final String image;
+  private final String profileImageURL;
 
-  public UserUpdateRequest(String username, String image) {
+  public UserUpdateRequest(String username, String profileImageURL) {
 
     this.username = username;
-    this.image = image;
+    this.profileImageURL = profileImageURL;
   }
 
   public String getUsername() {
     return username;
   }
 
-  public String getImage() {
-    return image;
+  public String getProfileImageURL() {
+    return profileImageURL;
   }
 }
