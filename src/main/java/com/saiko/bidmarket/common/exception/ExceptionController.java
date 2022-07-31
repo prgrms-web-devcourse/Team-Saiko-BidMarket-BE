27445bb5b7c874e954f0026de17d3b7f10b3d092
UJ15 <dbwogml15@naver.com>
2022-07-31 15:30:09 +0900

    [BM-88] :sparkles: ExceptionController exception 추가

--- src/main/java/com/saiko/bidmarket/common/exception/ExceptionController.java
+++ src/main/java/com/saiko/bidmarket/common/exception/ExceptionController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,15 +13,38 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ExceptionController {
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+  @Order(1)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    logger.warn("MethodArgumentNotValidException : ", e);
+  }
+
+  @Order(2)
+  @ExceptionHandler(IllegalArgumentException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleIllegalArgumentException(IllegalArgumentException e) {
+    logger.warn("IllegalArgumentException : ", e);
+  }
+
+  @Order(3)
   @ExceptionHandler(NotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public void handleNotFoundException(NotFoundException e) {
     logger.warn("NotFoundException : ", e);
   }
 
-  @ExceptionHandler(IllegalArgumentException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public void handleIllegalArgumentException(IllegalArgumentException e) {
-    logger.warn("IllegalArgumentException : ", e);
+  @Order(4)
+  @ExceptionHandler(RuntimeException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public void handleRuntimeException(RuntimeException e) {
+    logger.error("RuntimeException : ", e);
+  }
+
+  @Order(5)
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public void handleException(Exception e) {
+    logger.error("Exception : ", e);
   }
 }
