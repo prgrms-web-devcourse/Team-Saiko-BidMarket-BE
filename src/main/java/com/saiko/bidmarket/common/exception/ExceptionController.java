78266c90d6f772430ee7b62111ace22c5c18429f
UJ15 <dbwogml15@naver.com>
2022-07-31 16:21:26 +0900

    [BM-88] :sparkles: valid exception들 추가

--- src/main/java/com/saiko/bidmarket/common/exception/ExceptionController.java
+++ src/main/java/com/saiko/bidmarket/common/exception/ExceptionController.java
@@ -1,9 +1,11 @@
 package com.saiko.bidmarket.common.exception;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.annotation.Order;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,42 +16,54 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 public class ExceptionController {
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-  @Order(1)
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
     logger.warn("MethodArgumentNotValidException : ", e);
   }
 
-  @Order(2)
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public void handleMethodArgumentNotValidException(MethodArgumentTypeMismatchException e) {
     logger.warn("MethodArgumentTypeMismatchException : ", e);
   }
 
-  @Order(3)
+  @ExceptionHandler(ConversionFailedException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleConversionFailedException(ConversionFailedException e) {
+    logger.warn("ConversionFailedException : ", e);
+  }
+
+  @ExceptionHandler(NumberFormatException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleNumberFormatException(NumberFormatException e) {
+    logger.warn("NumberFormatException : ", e);
+  }
+
+  @ExceptionHandler(BindException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public void handleBindException(BindException e) {
+    logger.warn("BindException : ", e);
+  }
+
   @ExceptionHandler(IllegalArgumentException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public void handleIllegalArgumentException(IllegalArgumentException e) {
     logger.warn("IllegalArgumentException : ", e);
   }
 
-  @Order(4)
   @ExceptionHandler(NotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public void handleNotFoundException(NotFoundException e) {
     logger.warn("NotFoundException : ", e);
   }
 
-  @Order(5)
   @ExceptionHandler(RuntimeException.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public void handleRuntimeException(RuntimeException e) {
     logger.error("RuntimeException : ", e);
   }
 
-  @Order(6)
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public void handleException(Exception e) {
