36ad0212f5f4fa8d9ecb895c32f5a8bf294266a3
waterfog9580 <waterfog9580@gmail.com>
2022-08-01 22:11:32 +0900

    [BM-168] :sparkles: 채팅관련 엔티티 추가

---
+++ src/main/java/com/saiko/bidmarket/chat/entity/ChatRoom.java
@@ -0,0 +1,32 @@
+package com.saiko.bidmarket.chat.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.saiko.bidmarket.user.entity.User;
+
+@Entity
+public class ChatRoom {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  private User seller;
+
+  @ManyToOne
+  private User customer;
+
+  @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<ChatMessage> chatMessage;
+
+}
+
