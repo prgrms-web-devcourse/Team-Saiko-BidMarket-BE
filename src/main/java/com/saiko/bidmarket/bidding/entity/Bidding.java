b99706c033d45875f40dcc29566b286cbe8e4448
dojinyou <djyou128@gmail.com>
2022-08-01 17:42:28 +0900

    [BM-129] :lipstick: 가독성을 위한 개행 문자 추가

--- src/main/java/com/saiko/bidmarket/bidding/entity/Bidding.java
+++ src/main/java/com/saiko/bidmarket/bidding/entity/Bidding.java
@@ -5,7 +5,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
@@ -17,7 +16,9 @@ import com.saiko.bidmarket.user.entity.User;
 
 @Entity
 public class Bidding extends BaseTime {
+
   private static final long BASE_PRICE = 1000;
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
