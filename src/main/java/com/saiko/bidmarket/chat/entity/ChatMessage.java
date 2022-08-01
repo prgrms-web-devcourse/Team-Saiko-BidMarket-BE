4ce3e39bbd05a66b6973c8a5b65d173e6ce198b5
waterfog9580 <waterfog9580@gmail.com>
2022-08-01 22:11:32 +0900

    [BM-168] :sparkles: 채팅관련 엔티티 추가

---
+++ src/main/java/com/saiko/bidmarket/chat/entity/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.saiko.bidmarket.chat.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.saiko.bidmarket.user.entity.User;
+
+@Entity
+public class ChatMessage {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private User sender;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private ChatRoom chatRoom;
+
+  @NotBlank
+  @Length(max = 2000)
+  private String message;
+
+}
---
+++ src/main/resources/sql/chat/chat_schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `chat_message` CASCADE;
+DROP TABLE IF EXISTS `chat_room` CASCADE;
+
+create table `chat_message`
+(
+    id           bigint        not null,
+    sender_id    bigint        not null,
+    chat_room_id bigint        not null,
+    message      varchar(2000) not null,
+    created_at   timestamp,
+    updated_at   timestamp
+);
+
+create table `chat_room`
+(
+    id          bigint not null,
+    seller_id   bigint not null,
+    customer_id bigint not null,
+    created_at  timestamp,
+    updated_at  timestamp
+);
