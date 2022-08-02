0da3b01a5e5193e6785e8b5d307ad58f65110c76
waterfog9580 <waterfog9580@gmail.com>
2022-08-03 03:01:38 +0900

    [BM-186] :sparkles: getUserProducts 프로덕션 코드 작성

--- src/main/java/com/saiko/bidmarket/user/service/UserService.java
+++ src/main/java/com/saiko/bidmarket/user/service/UserService.java
@@ -1,7 +1,11 @@
 package com.saiko.bidmarket.user.service;
 
+import java.util.List;
+
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import com.saiko.bidmarket.user.controller.dto.UserProductSelectRequest;
+import com.saiko.bidmarket.user.controller.dto.UserProductSelectResponse;
 import com.saiko.bidmarket.user.controller.dto.UserSelectResponse;
 import com.saiko.bidmarket.user.controller.dto.UserUpdateRequest;
 import com.saiko.bidmarket.user.entity.User;
@@ -14,4 +18,6 @@ public interface UserService {
   UserSelectResponse findById(long id);
 
   void updateUser(long id, UserUpdateRequest request);
+
+  List<UserProductSelectResponse> findAllUserProducts(long userId, UserProductSelectRequest request);
 }
