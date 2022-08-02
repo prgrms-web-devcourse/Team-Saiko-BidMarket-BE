7acf1cd6770c3aeb0a7aa46ccb6046f8cf083a88
dojinyou <djyou128@gmail.com>
2022-08-03 03:03:33 +0900

    [BM-84] :recycle: 롬복 추가 및 repository 적용

--- src/main/java/com/saiko/bidmarket/bidding/controller/dto/BiddingCreateResponse.java
+++ src/main/java/com/saiko/bidmarket/bidding/controller/dto/BiddingCreateResponse.java
@@ -1,14 +1,12 @@
 package com.saiko.bidmarket.bidding.controller.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class BiddingCreateResponse {
+
   private final long id;
-
-  public BiddingCreateResponse(long id) {
-    this.id = id;
-  }
-
-  public long getId() {
-    return id;
-  }
 
 }
