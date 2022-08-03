63279995612ba993d6014223b01343ecd278739b
gkdud583 <hyng0115@gmail.com>
2022-08-04 01:00:40 +0900

    [BM-189] :adhesive_bandage: ddl 스크립트 수정

--- src/main/resources/sql/bidding/bidding_schema.sql
+++ src/main/resources/sql/bidding/bidding_schema.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS `bidding` CASCADE;
 
 CREATE TABLE `bidding`
 (
-    id              bigint       not null,
-    bidding_price   bigint       not null,
-    won             tinyint(1)   not null,
-    created_at      timestamp    not null,
-    updated_at      timestamp    not null,
-    bidder_id       bigint       not null,
-    product_id      bigint       not null
+    id            bigint     not null,
+    bidding_price bigint     not null,
+    won           tinyint(1) not null,
+    created_at    timestamp  not null,
+    updated_at    timestamp,
+    bidder_id     bigint     not null,
+    product_id    bigint     not null
 );
