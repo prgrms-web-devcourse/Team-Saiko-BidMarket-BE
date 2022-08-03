5c82b2ce35dbcaa177fe774ce2fad23d09579938
ecvheo1 <ecvheo@naver.com>
2022-08-03 13:19:06 +0900

    [BM-67] :sparkles: 종료 시킬 경매 리스트 조회 메서드 구현

--- src/main/java/com/saiko/bidmarket/product/repository/ProductRepository.java
+++ src/main/java/com/saiko/bidmarket/product/repository/ProductRepository.java
@@ -1,8 +1,14 @@
 package com.saiko.bidmarket.product.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.saiko.bidmarket.product.entity.Product;
 
 public interface ProductRepository extends ProductCustomRepository, JpaRepository<Product, Long> {
+
+  List<Product> findAllByProgressedAndExpireAtGreaterThanEqualAndExpireAtLessThan(
+      boolean progressed, LocalDateTime start, LocalDateTime end);
 }
