da63a6fa5dea08281b5a67bb318360babd206ede
UJ15 <dbwogml15@naver.com>
2022-08-03 15:22:11 +0900

    [BM-181] :sparkles: UserSelectResponseDto에 인코딩된 ID 필드 추가

--- src/main/java/com/saiko/bidmarket/user/controller/dto/UserSelectResponse.java
+++ src/main/java/com/saiko/bidmarket/user/controller/dto/UserSelectResponse.java
@@ -1,17 +1,25 @@
 package com.saiko.bidmarket.user.controller.dto;
 
+import com.saiko.bidmarket.common.util.IdEncoder;
 import com.saiko.bidmarket.user.entity.User;
 
 public class UserSelectResponse {
+
+  private final String encodedUserId;
 
   private final String username;
 
   private final String profileImageUrl;
 
-  public UserSelectResponse(String username, String profileImageUrl) {
+  public UserSelectResponse(String encodedUserId, String username, String profileImageUrl) {
 
+    this.encodedUserId = encodedUserId;
     this.username = username;
     this.profileImageUrl = profileImageUrl;
+  }
+
+  public String getEncodedUserId() {
+    return encodedUserId;
   }
 
   public String getUsername() {
@@ -23,6 +31,7 @@ public class UserSelectResponse {
   }
 
   public static UserSelectResponse from(User user) {
-    return new UserSelectResponse(user.getUsername(), user.getProfileImage());
+    final String encodedUserId = IdEncoder.encode(user.getId());
+    return new UserSelectResponse(encodedUserId, user.getUsername(), user.getProfileImage());
   }
 }
