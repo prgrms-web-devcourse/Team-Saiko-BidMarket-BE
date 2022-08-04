1ad41da6beaaacfd84117eeb997b8b98e75d45d5
UJ15 <dbwogml15@naver.com>
2022-08-04 20:05:22 +0900

    [BM-208] :recycle: cors 코드 수정정

--- src/main/java/com/saiko/bidmarket/common/config/WebSecurityConfig.java
+++ src/main/java/com/saiko/bidmarket/common/config/WebSecurityConfig.java
@@ -107,7 +107,7 @@ public class WebSecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.addAllowedOrigin("*");
+    configuration.addAllowedOriginPattern("*");
     configuration.addAllowedHeader("*");
     configuration.addAllowedMethod("*");
     configuration.setAllowCredentials(true);
