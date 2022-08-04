f706a8434e31da07fbced703454296f3731e6f54
dojinyou <djyou128@gmail.com>
2022-08-04 18:22:41 +0900

    [BM-207] :wrench: 테스트용 샘플 데이터 추가

--- src/main/resources/sql/user/user_data.sql
+++ src/main/resources/sql/user/user_data.sql
@@ -15,3 +15,11 @@ VALUES (1, 1, 1),
        (2, 2, 1),
        (3, 2, 2)
 ;
+
+INSERT INTO `user`(username, provider, provider_id, profile_image, group_id, created_at)
+VALUES ('유재희', 'google', '113052825681484000000',
+        'https://lh3.googleusercontent.com/a/AItbvmmujg3pE4C3iRbHWRZCd-BtvUykZ2BaaIAuSoo7=s96-c', 1,
+        '2022-07-03T18:06:15'),
+       ('abramgech', 'google', '103163611869308335152',
+        'https://lh3.googleusercontent.com/a/AItbvmmRBO7OpOF-F-6FrAY-lP8FHJOWwC1n_GNyaNsH=s96-c', 1,
+        '2022-07-04 17:23:07');
