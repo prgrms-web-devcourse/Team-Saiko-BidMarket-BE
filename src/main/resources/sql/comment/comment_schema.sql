0b0d4c4a8b9b5e37f8497401ee40486e2f2e9252
gkdud583 <hyng0115@gmail.com>
2022-08-06 12:18:18 +0900

    [BM-248] :adhesive_bandage: 스키마에 컬럼 추가

--- src/main/resources/sql/comment/comment_schema.sql
+++ src/main/resources/sql/comment/comment_schema.sql
@@ -5,5 +5,7 @@ CREATE TABLE `comment`
     id         bigint       not null,
     content    varchar(500) not null,
     product_id bigint       not null,
-    user_id    bigint       not null
+    user_id    bigint       not null,
+    created_at timestamp    not null,
+    updated_at timestamp
 );
