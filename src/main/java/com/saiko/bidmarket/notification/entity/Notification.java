45aa90f6c262412ae0750ecafd635733d7af5c93
ecvheo1 <ecvheo@naver.com>
2022-08-06 15:14:26 +0900

    [BM-252] :sparkles: 알림 객체 정의

---
+++ src/main/java/com/saiko/bidmarket/notification/entity/Notification.java
@@ -0,0 +1,54 @@
+package com.saiko.bidmarket.notification.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.util.Assert;
+
+import com.saiko.bidmarket.common.entity.BaseTime;
+import com.saiko.bidmarket.product.entity.Product;
+import com.saiko.bidmarket.user.entity.User;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTime {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @Column(length = 100)
+  private String message;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @Builder
+  public Notification(String message, Product product, User user) {
+    Assert.hasText(message, "Message must be provided");
+    Assert.notNull(product, "Product must be provided");
+    Assert.notNull(user, "User must be provided");
+
+    this.message = message;
+    this.product = product;
+    this.user = user;
+  }
+}
