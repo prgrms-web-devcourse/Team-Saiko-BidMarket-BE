aae3d76281e69cb591508c79632e2e449f0b6ae8
gkdud583 <hyng0115@gmail.com>
2022-08-06 11:26:25 +0900

    [BM-248] :sparkles: 댓글 엔티티 구현

---
+++ src/main/java/com/saiko/bidmarket/comment/entity/Comment.java
@@ -0,0 +1,52 @@
+package com.saiko.bidmarket.comment.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.util.Assert;
+
+import com.saiko.bidmarket.common.entity.BaseTime;
+import com.saiko.bidmarket.product.entity.Product;
+import com.saiko.bidmarket.user.entity.User;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTime {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @NotNull
+  @Column(length = 500)
+  private String content;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User writer;
+
+  @Builder
+  public Comment(String content, Product product, User writer) {
+    Assert.hasText(content, "Content must be provided");
+    Assert.notNull(product, "Product must be provided");
+    Assert.notNull(writer, "Writer must be provided");
+
+    this.content = content;
+    this.product = product;
+    this.writer = writer;
+  }
+}
