165789d51ef416460e62f7829d51037450926ef9
gkdud583 <hyng0115@gmail.com>
2022-08-06 21:12:18 +0900

    [BM-249] :adhesive_bandage: userId 타입 변경

--- src/main/java/com/saiko/bidmarket/comment/service/CommentService.java
+++ src/main/java/com/saiko/bidmarket/comment/service/CommentService.java
@@ -2,7 +2,8 @@ package com.saiko.bidmarket.comment.service;
 
 import com.saiko.bidmarket.comment.controller.dto.CommentCreateRequest;
 import com.saiko.bidmarket.comment.controller.dto.CommentCreateResponse;
+import com.saiko.bidmarket.common.entity.UnsignedLong;
 
 public interface CommentService {
-  CommentCreateResponse create(long userId, CommentCreateRequest request);
+  CommentCreateResponse create(UnsignedLong userId, CommentCreateRequest request);
 }
