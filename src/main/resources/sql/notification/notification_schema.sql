be4c8e697b215a39dd0eba36db9c15a4b17860b3
ecvheo1 <ecvheo@naver.com>
2022-08-06 15:14:56 +0900

    [BM-252] :memo: notification 스키마 작성 및 제약조건, 설정파일 수정

---
+++ src/main/resources/sql/notification/notification_schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `notification` CASCADE;
+
+CREATE TABLE `notification`
+(
+    id         bigint       not null,
+    message    varchar(100) not null,
+    product_id bigint       not null,
+    user_id    bigint       not null,
+    created_at timestamp    not null,
+    updated_at timestamp
+);
