3598d64f969cbaeaf86327223c5378062696dec2
waterfog9580 <waterfog9580@gmail.com>
2022-08-07 15:59:41 +0900

    [BM-229] :adhesive_bandage: stomp origin 주소 변경

--- src/main/java/com/saiko/bidmarket/common/config/WebSocketConfig.java
+++ src/main/java/com/saiko/bidmarket/common/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws-stomp")
-            .setAllowedOrigins("*")
+            .setAllowedOrigins("https://bidmarket.vercel.app/")
             .withSockJS();
   }
 
