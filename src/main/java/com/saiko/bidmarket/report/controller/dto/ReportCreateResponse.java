d83cdeb50b36d4567047a05a5eec6e4aa6949d9e
dojinyou <djyou128@gmail.com>
2022-08-07 23:33:34 +0900

    [BM-255] :sparkles: 신고 컨트롤러 및 신고 생성 기능 구현

---
+++ src/main/java/com/saiko/bidmarket/report/controller/dto/ReportCreateResponse.java
@@ -0,0 +1,10 @@
+package com.saiko.bidmarket.report.controller.dto;
+
+import com.saiko.bidmarket.common.entity.BaseCreateResponse;
+import com.saiko.bidmarket.common.entity.UnsignedLong;
+
+public class ReportCreateResponse extends BaseCreateResponse {
+  public ReportCreateResponse(UnsignedLong id) {
+    super(id);
+  }
+}
