4738ff845a89c9701d0bbdf62c33a9afd02bb9a7
waterfog9580 <waterfog9580@gmail.com>
2022-08-07 11:48:25 +0900

    [BM-229] :sparkles: builder 추가

--- src/main/java/com/saiko/bidmarket/chat/entity/ChatMessage.java
+++ src/main/java/com/saiko/bidmarket/chat/entity/ChatMessage.java
@@ -9,11 +9,19 @@ import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.util.Assert;
 
 import com.saiko.bidmarket.common.entity.BaseTime;
 import com.saiko.bidmarket.user.entity.User;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatMessage extends BaseTime {
 
   @Id
@@ -27,7 +35,17 @@ public class ChatMessage extends BaseTime {
   private ChatRoom chatRoom;
 
   @NotBlank
-  @Length(max = 2000)
+  @Length(min = 1, max = 2000)
   private String message;
 
+  @Builder(access = AccessLevel.PUBLIC)
+  private ChatMessage(User sender, ChatRoom chatRoom, String message) {
+    Assert.notNull(sender, "Sender must be provided");
+    Assert.notNull(chatRoom, "ChatRoom must be provided");
+    Assert.hasText(message, "Message must be provided");
+
+    this.sender = sender;
+    this.chatRoom = chatRoom;
+    this.message = message;
+  }
 }
--- src/main/java/com/saiko/bidmarket/chat/entity/ChatRoom.java
+++ src/main/java/com/saiko/bidmarket/chat/entity/ChatRoom.java
@@ -3,31 +3,48 @@ package com.saiko.bidmarket.chat.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import org.springframework.util.Assert;
+
 import com.saiko.bidmarket.common.entity.BaseTime;
 import com.saiko.bidmarket.user.entity.User;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoom extends BaseTime {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
   private User seller;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
   private User buyer;
 
   @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<ChatMessage> chatMessage;
 
+  @Builder
+  private ChatRoom(User seller, User buyer) {
+    Assert.notNull(seller, "Seller must be provided");
+    Assert.notNull(buyer, "Buyer must be provided");
+
+    this.seller = seller;
+    this.buyer = buyer;
+  }
 }
 
