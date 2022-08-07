e6e84ff40cfe18515026ea15c75a7b4157f59868
dojinyou <djyou128@gmail.com>
2022-08-07 19:02:43 +0900

    [BM-260] :white_check_mark: 신고서비스 신고하기 기능 테스트 작성

---
+++ src/test/java/com/saiko/bidmarket/report/service/DefaultReportServiceTest.java
@@ -0,0 +1,230 @@
+package com.saiko.bidmarket.report.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.saiko.bidmarket.common.entity.UnsignedLong;
+import com.saiko.bidmarket.common.exception.NotFoundException;
+import com.saiko.bidmarket.report.entity.Report;
+import com.saiko.bidmarket.report.repository.ReportRepository;
+import com.saiko.bidmarket.report.service.dto.ReportCreateDto;
+import com.saiko.bidmarket.user.entity.Group;
+import com.saiko.bidmarket.user.entity.User;
+import com.saiko.bidmarket.user.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+class DefaultReportServiceTest {
+
+  @InjectMocks
+  private DefaultReportService reportService;
+
+  @Mock
+  private ReportRepository reportRepository;
+
+  @Mock
+  private UserRepository userRepository;
+
+  private static final long requestUserId = 1L;
+
+  private static final String reason = "reason";
+
+  private static final User fromUser = new User("fromUser",
+                                                "imageUrl",
+                                                "provider",
+                                                "providerId",
+                                                new Group());
+
+  private static final User toUser = new User("toUser",
+                                              "imageUrl",
+                                              "provider",
+                                              "providerId",
+                                              new Group());
+
+  @BeforeAll
+  static void setUpUsersId() {
+    ReflectionTestUtils.setField(fromUser, "id", requestUserId);
+    ReflectionTestUtils.setField(toUser, "id", requestUserId * 2);
+  }
+
+  @Nested
+  @DisplayName("create 메소드는")
+  class DescribeCreateMethod {
+
+    @Nested
+    @DisplayName("빈 CreateDTo가 들어오면")
+    class ContextNullCreateDto {
+
+      @Test
+      @DisplayName("IllegalArgumentException을 던진다")
+      void ItThrowsIllegalArgumentException() {
+        // given
+        ReportCreateDto createDto = null;
+
+        // when
+        // then
+        assertThatThrownBy(() -> reportService.create(createDto))
+            .isInstanceOf(IllegalArgumentException.class);
+
+      }
+    }
+
+    @Nested
+    @DisplayName("fromUser를 찾을 수 없는 경우")
+    class ContextNotFoundFromUser {
+
+      @Test
+      @DisplayName("NotFoundException을 던진다")
+      void ItReturnCreatedReportId() {
+        // given
+        ReportCreateDto createDto = ReportCreateDto
+            .builder()
+            .reason(reason)
+            .fromUserId(UnsignedLong.valueOf(fromUser.getId()))
+            .toUserId(UnsignedLong.valueOf(toUser.getId()))
+            .build();
+
+        given(userRepository.findById(fromUser.getId())).willReturn(Optional.empty());
+
+        // when
+        // then
+        assertThatThrownBy(() -> reportService.create(createDto))
+            .isInstanceOf(NotFoundException.class);
+
+      }
+    }
+
+    @Nested
+    @DisplayName("toUser를 찾을 수 없는 경우")
+    class ContextNotFoundToUser {
+
+      @Test
+      @DisplayName("NotFoundException을 던진다")
+      void ItReturnCreatedReportId() {
+        // given
+        ReportCreateDto createDto = ReportCreateDto
+            .builder()
+            .reason(reason)
+            .fromUserId(UnsignedLong.valueOf(fromUser.getId()))
+            .toUserId(UnsignedLong.valueOf(toUser.getId()))
+            .build();
+
+        given(userRepository.findById(fromUser.getId())).willReturn(Optional.of(fromUser));
+        given(userRepository.findById(toUser.getId())).willReturn(Optional.empty());
+
+        // when
+        // then
+        assertThatThrownBy(() -> reportService.create(createDto))
+            .isInstanceOf(NotFoundException.class);
+
+      }
+    }
+
+    @Nested
+    @DisplayName("fromUser가 이미 toUser를 신고한 경우")
+    class ContextExistSameReport {
+
+      @Test
+      @DisplayName("IllegalArgumentException을 던진다")
+      void ItThrowsIllegalArgumentException() {
+        // given
+        ReportCreateDto createDto = ReportCreateDto
+            .builder()
+            .reason(reason)
+            .fromUserId(UnsignedLong.valueOf(fromUser.getId()))
+            .toUserId(UnsignedLong.valueOf(toUser.getId()))
+            .build();
+
+        given(userRepository.findById(fromUser.getId())).willReturn(Optional.of(fromUser));
+        given(userRepository.findById(toUser.getId())).willReturn(Optional.of(toUser));
+        given(reportRepository.existsByFromUserAndToUser(fromUser, toUser))
+            .willReturn(true);
+
+        // when
+        // then
+        assertThatThrownBy(() -> reportService.create(createDto))
+            .isInstanceOf(IllegalArgumentException.class);
+
+      }
+    }
+
+    @Nested
+    @DisplayName("신고자와 피신고자가 같은 경우")
+    class ContextSameUserWithFromAndTo {
+
+      @Test
+      @DisplayName("IllegalArgumentException을 던진다.")
+      void ItThrowsIllegalArgumentException() {
+        // given
+        User toUser = fromUser;
+
+        ReportCreateDto createDto = ReportCreateDto
+            .builder()
+            .reason(reason)
+            .fromUserId(UnsignedLong.valueOf(fromUser.getId()))
+            .toUserId(UnsignedLong.valueOf(toUser.getId()))
+            .build();
+
+        given(userRepository.findById(fromUser.getId())).willReturn(Optional.of(fromUser));
+        given(userRepository.findById(toUser.getId())).willReturn(Optional.of(toUser));
+        given(reportRepository.existsByFromUserAndToUser(fromUser, toUser))
+            .willReturn(false);
+
+        // when
+        // then
+        assertThatThrownBy(() -> reportService.create(createDto))
+            .isInstanceOf(IllegalArgumentException.class);
+
+      }
+    }
+
+    @Nested
+    @DisplayName("정상적인 Create Dto가 인자로 들어올 경우")
+    class ContextValidCreateDto {
+
+      @Test
+      @DisplayName("생성된 Report의 Id를 반환한다.")
+      void ItReturnCreatedReportId() {
+        // given
+        ReportCreateDto createDto = ReportCreateDto
+            .builder()
+            .reason(reason)
+            .fromUserId(UnsignedLong.valueOf(fromUser.getId()))
+            .toUserId(UnsignedLong.valueOf(toUser.getId()))
+            .build();
+        UnsignedLong expected = UnsignedLong.valueOf(1L);
+
+        Report createdReport = Report.builder()
+                                     .reason(reason)
+                                     .fromUser(fromUser)
+                                     .toUser(toUser)
+                                     .build();
+        ReflectionTestUtils.setField(createdReport, "id", expected.getValue());
+
+        given(userRepository.findById(fromUser.getId())).willReturn(Optional.of(fromUser));
+        given(userRepository.findById(toUser.getId())).willReturn(Optional.of(toUser));
+        given(reportRepository.existsByFromUserAndToUser(fromUser, toUser))
+            .willReturn(false);
+        given(reportRepository.save(any(Report.class))).willReturn(createdReport);
+
+        // when
+        UnsignedLong actual = reportService.create(createDto);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+
+      }
+    }
+  }
+}
