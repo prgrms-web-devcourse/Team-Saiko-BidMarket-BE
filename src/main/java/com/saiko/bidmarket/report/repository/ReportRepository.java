3c598954808de9f9948ca55998bac95114de99f1
dojinyou <djyou128@gmail.com>
2022-08-07 19:01:16 +0900

    [BM-260] :sparkles: 신고하기 기능을 위한 신고 레포지토리 구현

---
+++ src/main/java/com/saiko/bidmarket/report/repository/ReportRepository.java
@@ -0,0 +1,13 @@
+package com.saiko.bidmarket.report.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.saiko.bidmarket.common.entity.UnsignedLong;
+import com.saiko.bidmarket.report.entity.Report;
+import com.saiko.bidmarket.user.entity.User;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, UnsignedLong> {
+  boolean existsByFromUserAndToUser(User fromUser, User toUser);
+}
