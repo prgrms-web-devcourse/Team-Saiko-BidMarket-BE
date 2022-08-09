32b1f5067228282f486d9e92ca4e4ba1fa89c4ab
ecvheo1 <ecvheo@naver.com>
2022-08-09 20:36:39 +0900

    [BM-220] :sparkles: 알림 전체 조회 Service 구현

--- src/main/java/com/saiko/bidmarket/notification/service/DefaultNotificationService.java
+++ src/main/java/com/saiko/bidmarket/notification/service/DefaultNotificationService.java
@@ -1,19 +1,34 @@
 package com.saiko.bidmarket.notification.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import com.saiko.bidmarket.common.entity.UnsignedLong;
 import com.saiko.bidmarket.notification.controller.dto.NotificationSelectRequest;
 import com.saiko.bidmarket.notification.controller.dto.NotificationSelectResponse;
+import com.saiko.bidmarket.notification.repository.NotificationRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
 public class DefaultNotificationService implements NotificationService {
+
+  private final NotificationRepository notificationRepository;
 
   @Override
   public List<NotificationSelectResponse> findAllNotifications(UnsignedLong userId,
                                                                NotificationSelectRequest request) {
-    return null;
+    Assert.notNull(userId, "UserId must be provided");
+    Assert.notNull(request, "Request must be provided");
+
+    return notificationRepository.findAllNotification(userId, request)
+                            .stream()
+                            .map((NotificationSelectResponse::from))
+                            .collect(Collectors.toList());
   }
 }
