de759091c4e1901a3139bdc7ca3b646dd92091eb
ecvheo1 <ecvheo@naver.com>
2022-08-09 10:49:41 +0900

    [BM-288] :beers: 알림 메세지 내용 수정

--- src/main/java/com/saiko/bidmarket/notification/NotificationType.java
+++ src/main/java/com/saiko/bidmarket/notification/NotificationType.java
@@ -1,10 +1,10 @@
 package com.saiko.bidmarket.notification;
 
 public enum NotificationType {
-  END_PRODUCT_FOR_WRITER_WITH_WINNER("입찰 종료", "상품의 입찰 기간이 종료되었습니다. 낙찰자와 거래 일정을 잡아보세요!"),
-  END_PRODUCT_FOR_WRITER_NOT_WITH_WINNER("입찰 종료", "상품의 입찰 기간이 종료되었습니다. 재등록하시겠습니까?"),
-  END_PRODUCT_FOR_WINNER("입찰 종료", "상품의 입찰 기간이 종료되었습니다. 낙찰을 축하드립니다. 판매자와 거래 일정을 잡아보세요!"),
-  END_PRODUCT_FOR_BIDDER("입찰 종료", "상품의 입찰 기간이 종료되었습니다. 아쉽지만 낙찰되지 않으셨습니다.");
+  END_PRODUCT_FOR_WRITER_WITH_WINNER("입찰 종료", "등록하신 상품이 판매되었습니다. 낙찰자분과 채팅을 시작해보세요!"),
+  END_PRODUCT_FOR_WRITER_NOT_WITH_WINNER("입찰 종료", "아쉽게도 상품이 판매되지 않았어요. :( 상품을 재등록하시겠어요?"),
+  END_PRODUCT_FOR_WINNER("입찰 종료", "축하드립니다! 낙찰에 성공하신 제품이 있네요!"),
+  END_PRODUCT_FOR_BIDDER("입찰 종료", "아쉽게도 이번에는 낙찰 받지 못하셨어요. :( 다음에 다시 도전해주세요.");
 
   private final String type;
   private final String message;
