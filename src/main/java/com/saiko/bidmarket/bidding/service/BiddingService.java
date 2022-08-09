b47f4d68f4ef82e0085dea2b576baa58367b303b
dojinyou <djyou128@gmail.com>
2022-08-09 13:51:31 +0900

    [BM-282] :sparkles: 비딩 서비스 비딩 금액 조회 기능 구현

--- src/main/java/com/saiko/bidmarket/bidding/service/BiddingService.java
+++ src/main/java/com/saiko/bidmarket/bidding/service/BiddingService.java
@@ -1,11 +1,14 @@
 package com.saiko.bidmarket.bidding.service;
 
+import com.saiko.bidmarket.bidding.entity.BiddingPrice;
 import com.saiko.bidmarket.bidding.service.dto.BiddingCreateDto;
+import com.saiko.bidmarket.bidding.service.dto.BiddingPriceFindingDto;
 import com.saiko.bidmarket.common.entity.UnsignedLong;
-import com.saiko.bidmarket.product.entity.Product;
 
 public interface BiddingService {
 
   UnsignedLong create(BiddingCreateDto createDto);
+
+  BiddingPrice findBiddingPriceByProductIdAndUserId(BiddingPriceFindingDto biddingPriceFindingDto);
 
 }
