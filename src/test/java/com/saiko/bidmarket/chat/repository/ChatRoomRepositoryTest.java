59fdd992128f56b4b74309804ca60475515d76a5
waterfog9580 <waterfog9580@gmail.com>
2022-08-10 18:28:57 +0900

    [BM-281] :white_check_mark: findAllByUserId 테스트 작성

--- src/test/java/com/saiko/bidmarket/chat/repository/ChatRoomRepositoryTest.java
+++ src/test/java/com/saiko/bidmarket/chat/repository/ChatRoomRepositoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.saiko.bidmarket.chat.controller.dto.ChatRoomSelectRequest;
 import com.saiko.bidmarket.chat.entity.ChatRoom;
 import com.saiko.bidmarket.common.config.QueryDslConfig;
 import com.saiko.bidmarket.product.Category;
@@ -46,6 +47,7 @@ public class ChatRoomRepositoryTest {
   void deleteAll() {
     productRepository.deleteAll();
     userRepository.deleteAll();
+    chatRoomRepository.deleteAll();
   }
 
   @Nested
@@ -80,7 +82,45 @@ public class ChatRoomRepositoryTest {
     }
   }
 
-  private User getUser(String providerId, Group group) {
+  @Nested
+  @DisplayName("findAllByUserId 메서드는")
+  class DescribeFindAllByUserId {
+
+    @Test
+    @DisplayName("해당 유저가 속해있는 모든 채팅방을 반환한다")
+    void ItReturnAllChatRoomThatJoinInto() {
+      //given
+      @SuppressWarnings("all")
+      Group group = groupRepository
+          .findById(1L)
+          .get();
+
+      User user1 = userRepository.save(getUser("1", group));
+      User user2 = userRepository.save(getUser("2", group));
+      User user3 = userRepository.save(getUser("3", group));
+
+      Product product1 = productRepository.save(getProduct(user1));
+      Product product2 = productRepository.save(getProduct(user2));
+      Product product3 = productRepository.save(getProduct(user1));
+
+      chatRoomRepository.save(getChatRoom(user1, user2, product1));
+      chatRoomRepository.save(getChatRoom(user2, user3, product2));
+      chatRoomRepository.save(getChatRoom(user1, user3, product3));
+
+      ChatRoomSelectRequest request = new ChatRoomSelectRequest(0, 10);
+
+      //when
+      List<ChatRoom> chatRooms = chatRoomRepository.findAllByUserId(user2.getId(), request);
+
+      //then
+      assertThat(chatRooms.size()).isEqualTo(2);
+    }
+  }
+
+  private User getUser(
+      String providerId,
+      Group group
+  ) {
     return User
         .builder()
         .username("제로")
@@ -104,7 +144,11 @@ public class ChatRoomRepositoryTest {
         .build();
   }
 
-  private ChatRoom getChatRoom(User seller, User winner, Product product) {
+  private ChatRoom getChatRoom(
+      User seller,
+      User winner,
+      Product product
+  ) {
     return ChatRoom
         .builder()
         .seller(seller)
