811ec83125c1baf969f2f80667440a9638567071
ecvheo1 <ecvheo@naver.com>
2022-08-10 17:42:01 +0900

    [BM-226] :sparkles: 찜하기 엔티티 정의

---
+++ src/main/java/com/saiko/bidmarket/heart/entity/Heart.java
@@ -0,0 +1,47 @@
+package com.saiko.bidmarket.heart.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.util.Assert;
+
+import com.saiko.bidmarket.common.entity.BaseTime;
+import com.saiko.bidmarket.product.entity.Product;
+import com.saiko.bidmarket.user.entity.User;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Heart extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id")
+  private Product product;
+
+  @Builder
+  public Heart(User user, Product product) {
+    Assert.notNull(user, "User must be provided");
+    Assert.notNull(product, "Product must be provided");
+
+    this.user = user;
+    this.product = product;
+  }
+}
