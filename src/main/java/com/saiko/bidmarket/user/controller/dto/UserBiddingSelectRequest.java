fb510e8b56fa697d03be17b16668f804c407d917
gkdud583 <hyng0115@gmail.com>
2022-08-10 15:06:03 +0900

    [BM-293] :recycle: offset long 타입으로 변경

--- src/main/java/com/saiko/bidmarket/user/controller/dto/UserBiddingSelectRequest.java
+++ src/main/java/com/saiko/bidmarket/user/controller/dto/UserBiddingSelectRequest.java
@@ -12,14 +12,14 @@ import lombok.Getter;
 @Getter
 public class UserBiddingSelectRequest {
   @PositiveOrZero
-  private final int offset;
+  private final long offset;
 
   @Positive
   private final int limit;
 
   private final Sort sort;
 
-  public UserBiddingSelectRequest(int offset, int limit, Sort sort) {
+  public UserBiddingSelectRequest(long offset, int limit, Sort sort) {
     this.offset = offset;
     this.limit = limit;
     this.sort = sort != null ? sort : END_DATE_ASC;
