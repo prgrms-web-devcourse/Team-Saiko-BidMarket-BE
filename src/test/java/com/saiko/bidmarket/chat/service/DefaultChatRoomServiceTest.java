8883600ab007b211c0b9e797f5e350efbc5b3653
waterfog9580 <waterfog9580@gmail.com>
2022-08-10 20:33:49 +0900

    [BM-281] :white_check_mark: 채팅방 목록 조회 서비스 테스트 작성

--- src/test/java/com/saiko/bidmarket/chat/service/DefaultChatRoomServiceTest.java
+++ src/test/java/com/saiko/bidmarket/chat/service/DefaultChatRoomServiceTest.java
@@ -4,17 +4,22 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import com.saiko.bidmarket.chat.controller.dto.ChatRoomSelectRequest;
+import com.saiko.bidmarket.chat.controller.dto.ChatRoomSelectResponse;
 import com.saiko.bidmarket.chat.entity.ChatRoom;
 import com.saiko.bidmarket.chat.repository.ChatRoomRepository;
 import com.saiko.bidmarket.chat.service.dto.ChatRoomCreateParam;
@@ -96,28 +101,111 @@ class DefaultChatRoomServiceTest {
     }
   }
 
+  @Nested
+  @DisplayName("findAll메서드는")
+  class DescribeFindAll {
+
+    @Nested
+    @DisplayName("user id 가 음수이면")
+    class ContextWithNonPositiveUserId {
+
+      @ParameterizedTest
+      @ValueSource(longs = {0, -1, Long.MIN_VALUE})
+      @DisplayName("IllegalArgumentException 에러를 발생시킨다")
+      void ItThrowsIllegalArgumentException(long userId) {
+        //when, then
+        ChatRoomSelectRequest request = new ChatRoomSelectRequest(1, 10);
+        assertThatThrownBy(() -> chatRoomService.findAll(userId, request));
+      }
+    }
+
+    @Nested
+    @DisplayName("request 가 null이면")
+    class ContextWithNullRequest {
+
+      @Test
+      @DisplayName("IllegalArgumentException 에러를 발생시킨다")
+      void ItThrowsIllegalArgumentException() {
+        //when, then
+        assertThatThrownBy(() -> chatRoomService.findAll(1L, null));
+      }
+    }
+
+    @Nested
+    @DisplayName("유효한 값이 전달되면")
+    class ContextWithValidParameter {
+
+      @Test
+      @DisplayName("해당 조건의 채팅방 목록을 반환한다")
+      void ItResponseChatRoomList() {
+        //given
+        long chatRoomId = 1L;
+        long sellerId = 1L;
+        long winnerId = 2L;
+        long productId = 1L;
+
+        User seller = getUser(sellerId);
+        User winner = getUser(winnerId);
+        Product product = getProduct(productId, sellerId);
+        ChatRoom chatRoom = getChatRoom(chatRoomId, seller, winner, product);
+
+        ChatRoomSelectRequest request = new ChatRoomSelectRequest(0, 10);
+
+
+        given(chatRoomRepository.findAllByUserId(anyLong(), any(ChatRoomSelectRequest.class)))
+            .willReturn(List.of(chatRoom));
+        //when
+        List<ChatRoomSelectResponse> responses = chatRoomService.findAll(sellerId, request);
+
+        //then
+        assertThat(responses.size()).isEqualTo(1);
+      }
+    }
+  }
+
+  private ChatRoom getChatRoom(
+      long chatRoomId,
+      User seller,
+      User winner,
+      Product product
+  ) {
+    ChatRoom chatRoom = ChatRoom
+        .builder()
+        .seller(seller)
+        .winner(winner)
+        .product(product)
+        .build();
+    ReflectionTestUtils.setField(chatRoom, "id", chatRoomId);
+    return chatRoom;
+  }
+
   private User getUser(long userId) {
-    User user = User.builder()
-                    .username("test")
-                    .provider("test")
-                    .providerId("test")
-                    .profileImage("test")
-                    .group(new Group())
-                    .build();
+    User user = User
+        .builder()
+        .username("test")
+        .provider("test")
+        .providerId("test")
+        .profileImage("test")
+        .group(new Group())
+        .build();
 
     ReflectionTestUtils.setField(user, "id", userId);
     return user;
   }
 
-  private Product getProduct(long productId, long userId) {
-    Product product = Product.builder()
-                             .title("test")
-                             .images(Collections.emptyList())
-                             .writer(getUser(userId))
-                             .description("test")
-                             .minimumPrice(1000)
-                             .category(Category.BEAUTY)
-                             .build();
+  private Product getProduct(
+      long productId,
+      long userId
+  ) {
+    Product product = Product
+        .builder()
+        .title("test")
+        .images(Collections.emptyList())
+        .writer(getUser(userId))
+        .description("test")
+        .minimumPrice(1000)
+        .category(Category.BEAUTY)
+        .build();
 
     ReflectionTestUtils.setField(product, "id", productId);
     return product;
