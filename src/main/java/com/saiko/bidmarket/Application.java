200acbe36d78491263ae6a82946c9f948eb79e74
UJ15 <dbwogml15@naver.com>
2022-08-12 18:40:14 +0900

    [BM-330] :construction_worker: 어플리케이션 시간 동기화

--- src/main/java/com/saiko/bidmarket/Application.java
+++ src/main/java/com/saiko/bidmarket/Application.java
@@ -1,5 +1,9 @@
 package com.saiko.bidmarket;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -18,4 +22,8 @@ public class Application {
     SpringApplication.run(Application.class, args);
   }
 
+  @PostConstruct
+  public void started(){
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+  }
 }
