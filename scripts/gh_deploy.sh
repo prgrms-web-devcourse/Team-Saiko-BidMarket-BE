9dcc527c5267b9ec25394c84276d3c20a4ccbf13
UJ15 <dbwogml15@naver.com>
2022-08-12 23:20:41 +0900

    [BM-330] :construction_worker: Application 시간 동기화

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -29,7 +29,7 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-sudo nohup java -jar -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+sudo nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=local $DEPLOY_JAR --server.port=8080 >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
 
 sleep 3
 
