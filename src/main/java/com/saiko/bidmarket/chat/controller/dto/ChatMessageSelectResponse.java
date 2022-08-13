006c3a7ead5188b89ebe795fdeff24b63fcd7623
waterfog9580 <waterfog9580@gmail.com>
2022-08-13 21:22:05 +0900

    [BM-345] :sparkles: 채팅 메시지 조회 api 필드 추가

--- src/main/java/com/saiko/bidmarket/chat/controller/dto/ChatMessageSelectResponse.java
+++ src/main/java/com/saiko/bidmarket/chat/controller/dto/ChatMessageSelectResponse.java
@@ -3,6 +3,7 @@ package com.saiko.bidmarket.chat.controller.dto;
 import java.time.LocalDateTime;
 
 import com.saiko.bidmarket.chat.entity.ChatMessage;
+import com.saiko.bidmarket.user.entity.User;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -12,17 +13,18 @@ import lombok.Getter;
 @Builder(access = AccessLevel.PRIVATE)
 public class ChatMessageSelectResponse {
 
-  private final long userId;
+  private final ChatUserInfo userInfo;
   private final String content;
   private final LocalDateTime createdAt;
 
   public static ChatMessageSelectResponse from(ChatMessage chatMessage) {
+    User sender = chatMessage.getSender();
+
     return ChatMessageSelectResponse
         .builder()
-        .userId(chatMessage.getSender().getId())
+        .userInfo(ChatUserInfo.from(sender))
         .content(chatMessage.getMessage())
         .createdAt(chatMessage.getCreatedAt())
         .build();
   }
-
 }
