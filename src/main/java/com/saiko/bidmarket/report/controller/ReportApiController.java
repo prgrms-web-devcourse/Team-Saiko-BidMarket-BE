a0f369df0cbf8d0774a2008f8e3e2049b2d24717
dojinyou <djyou128@gmail.com>
2022-08-15 01:20:24 +0900

    [BM-319] :recycle: api 변경에 따른 수정

--- src/main/java/com/saiko/bidmarket/report/controller/ReportApiController.java
+++ src/main/java/com/saiko/bidmarket/report/controller/ReportApiController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.saiko.bidmarket.common.jwt.JwtAuthentication;
 import com.saiko.bidmarket.report.controller.dto.ReportCreateRequest;
-import com.saiko.bidmarket.report.controller.dto.ReportCreateResponse;
-import com.saiko.bidmarket.report.entity.Report;
 import com.saiko.bidmarket.report.service.ReportService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,58 +23,17 @@ public class ReportApiController {
 
   private final ReportService reportService;
 
-  @PostMapping("users/{userId}")
+  @PostMapping()
   @ResponseStatus(HttpStatus.CREATED)
-  public ReportCreateResponse createToUser(
+  public void create(
       @AuthenticationPrincipal
       JwtAuthentication authentication,
-      @PathVariable
-      long userId,
       @RequestBody @Valid
       ReportCreateRequest createRequest
   ) {
-    return reportService.create(
+    reportService.create(
         authentication.getUserId(),
-        Report.Type.USER,
-        userId,
         createRequest
     );
   }
-
-  @PostMapping("products/{productId}")
-  @ResponseStatus(HttpStatus.CREATED)
-  public ReportCreateResponse createToProduct(
-      @AuthenticationPrincipal
-      JwtAuthentication authentication,
-      @PathVariable
-      long productId,
-      @RequestBody @Valid
-      ReportCreateRequest createRequest
-  ) {
-    return reportService.create(
-        authentication.getUserId(),
-        Report.Type.PRODUCT,
-        productId,
-        createRequest
-    );
-  }
-
-  @PostMapping("comments/{commentId}")
-  @ResponseStatus(HttpStatus.CREATED)
-  public ReportCreateResponse createToComment(
-      @AuthenticationPrincipal
-      JwtAuthentication authentication,
-      @PathVariable
-      long commentId,
-      @RequestBody @Valid
-      ReportCreateRequest createRequest
-  ) {
-    return reportService.create(
-        authentication.getUserId(),
-        Report.Type.COMMENT,
-        commentId,
-        createRequest
-    );
-  }
-
 }
--- src/main/java/com/saiko/bidmarket/report/controller/dto/ReportCreateRequest.java
+++ src/main/java/com/saiko/bidmarket/report/controller/dto/ReportCreateRequest.java
@@ -1,19 +1,22 @@
 package com.saiko.bidmarket.report.controller.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.saiko.bidmarket.report.entity.Report;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class ReportCreateRequest {
+
+  @NotNull
+  private final Report.Type type;
+
+  private final long typeId;
 
   @NotBlank
   private final String reason;
-
-  @JsonCreator
-  public ReportCreateRequest(String reason) {
-    this.reason = reason;
-  }
 }
